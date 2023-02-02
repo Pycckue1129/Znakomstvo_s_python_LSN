491fb1dea192dc0a1e64e02384b093d90f0de52b
Artem <burkun_artem@mail.ru>
2023-02-02 07:54:28 +0300

    first

---
+++ Lsn1/Python/HelloPython/lec.py
@@ -0,0 +1,37 @@
+print('hello world')
+
+# int float boolean str list
+value = None
+print(type(value))
+
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+
+value = 12334
+print(type(value))
+
+
+s = 'hello \nworld' #переход на новую строку
+
+print(s)
+print(a, '-' ,b, '-', s)
+print('{} - {} - {}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+
+f = True
+print(f)
+
+list = [1, '2', '3', 'hello', 2, True, 2.4]
+print(list)
+
+print('Введите а')
+a = input()
+print(a)
+
+print('Введите b')
+b = int(input())
+print(b,' + ', b, ' = ', b + b)
+
+
---
+++ Lsn1/Python/HelloPython/lec3.py
@@ -0,0 +1,19 @@
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#     print(a)
+# else:
+#     print(b)
+
+
+# list = [1,2,3,4,5]
+# for i in list:
+#     print(i**2)
+
+# r = range(10)
+# for i in r:
+#     print(i)
+
+for i in 'qwerty':
+    print(i)
+
